92791ec69894026dd81dc1f4b386c789749b8119
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2026-03-14 20:23:12 +0700

    membuat dto

---
+++ src/main/java/com/example/ktpapi/dto/KtpDto.java
@@ -0,0 +1,15 @@
+package com.example.ktpapi.dto;
+
+import lombok.Data;
+import java.util.Date;
+
+@Data
+public class KtpDto {
+
+    private Integer id;
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private Date tanggalLahir;
+    private String jenisKelamin;
+}
