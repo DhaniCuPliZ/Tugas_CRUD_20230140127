4a0ccca5f2935062d34e3a9d29849455bb18f1b2
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2026-03-15 04:53:16 +0700

    memperbaiki code FINAL

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,176 +1,183 @@
-const API = "http://localhost:8080/ktp";
+const API_URL = "/ktp";
 
 $(document).ready(function(){
 
-    loadData();
+loadData();
+
+$("#ktpForm").submit(function(e){
+
+e.preventDefault();
+
+const id = $("#id").val();
+
+const data = {
+nomorKtp: $("#nomorKtp").val(),
+namaLengkap: $("#namaLengkap").val(),
+alamat: $("#alamat").val(),
+tanggalLahir: $("#tanggalLahir").val(),
+jenisKelamin: $("#jenisKelamin").val()
+};
+
+if(id){
+updateData(id,data);
+}else{
+createData(data);
+}
 
 });
 
+});
+
+
+
 function loadData(){
 
-    $.ajax({
-        url: API,
-        type: "GET",
+$.ajax({
+url:API_URL,
+type:"GET",
 
-        success: function(response){
+success:function(res){
 
-            let rows = "";
+let rows="";
 
-            response.data.forEach(function(item){
+res.data.forEach(item=>{
 
-                rows += `
-                <tr>
-                    <td>${item.id}</td>
-                    <td>${item.nomorKtp}</td>
-                    <td>${item.namaLengkap}</td>
-                    <td>${item.alamat}</td>
-                    <td>${item.tanggalLahir}</td>
-                    <td>${item.jenisKelamin}</td>
-                    <td>
-                        <button onclick="editData(${item.id})">Edit</button>
-                        <button onclick="deleteData(${item.id})">Delete</button>
-                    </td>
-                </tr>
-                `;
+let tanggal = item.tanggalLahir.split("T")[0];
 
-            });
+rows+=`
+<tr>
+<td>${item.id}</td>
+<td>${item.nomorKtp}</td>
+<td>${item.namaLengkap}</td>
+<td>${item.alamat}</td>
+<td>${tanggal}</td>
+<td>${item.jenisKelamin}</td>
+<td>
+<button class="btn-edit" onclick="editData(${item.id})">Edit</button>
+<button class="btn-delete" onclick="deleteData(${item.id})">Delete</button>
+</td>
+</tr>
+`;
 
-            $("#tableData").html(rows);
+});
 
-        },
-
-        error:function(){
-            alert("Gagal mengambil data");
-        }
-
-    });
+$("#dataTable").html(rows);
 
 }
 
-function saveData(){
-
-    let id = $("#id").val();
-
-    let data = {
-        nomorKtp: $("#nomorKtp").val(),
-        namaLengkap: $("#namaLengkap").val(),
-        alamat: $("#alamat").val(),
-        tanggalLahir: $("#tanggalLahir").val(),
-        jenisKelamin: $("#jenisKelamin").val()
-    };
-
-    if(id == ""){
-
-        $.ajax({
-
-            url: API,
-            type: "POST",
-            contentType:"application/json",
-            data: JSON.stringify(data),
-
-            success:function(response){
-
-                alert(response.message);
-
-                clearForm();
-
-                loadData();
-
-            },
-
-            error:function(){
-                alert("Gagal menambah data");
-            }
-
-        });
-
-    }else{
-
-        $.ajax({
-
-            url: API + "/" + id,
-            type:"PUT",
-            contentType:"application/json",
-            data: JSON.stringify(data),
-
-            success:function(response){
-
-                alert(response.message);
-
-                clearForm();
-
-                loadData();
-
-            },
-
-            error:function(){
-                alert("Gagal update data");
-            }
-
-        });
-
-    }
+});
 
 }
+
+
+
+function createData(data){
+
+$.ajax({
+url:API_URL,
+type:"POST",
+contentType:"application/json",
+data:JSON.stringify(data),
+
+success:function(res){
+
+alert(res.message);
+
+resetForm();
+
+loadData();
+
+}
+
+});
+
+}
+
+
 
 function editData(id){
 
-    $.ajax({
+$.ajax({
+url:API_URL+"/"+id,
+type:"GET",
 
-        url: API + "/" + id,
-        type:"GET",
+success:function(res){
 
-        success:function(response){
+const d = res.data;
 
-            let data = response.data;
+$("#id").val(d.id);
+$("#nomorKtp").val(d.nomorKtp);
+$("#namaLengkap").val(d.namaLengkap);
+$("#alamat").val(d.alamat);
 
-            $("#id").val(data.id);
-            $("#nomorKtp").val(data.nomorKtp);
-            $("#namaLengkap").val(data.namaLengkap);
-            $("#alamat").val(data.alamat);
-            $("#tanggalLahir").val(data.tanggalLahir);
-            $("#jenisKelamin").val(data.jenisKelamin);
+let tanggal = d.tanggalLahir.split("T")[0];
+$("#tanggalLahir").val(tanggal);
 
-        }
-
-    });
+$("#jenisKelamin").val(d.jenisKelamin);
 
 }
+
+});
+
+}
+
+
+
+function updateData(id,data){
+
+$.ajax({
+url:API_URL+"/"+id,
+type:"PUT",
+contentType:"application/json",
+data:JSON.stringify(data),
+
+success:function(res){
+
+alert(res.message);
+
+resetForm();
+
+loadData();
+
+}
+
+});
+
+}
+
+
 
 function deleteData(id){
 
-    if(confirm("Yakin ingin menghapus data?")){
+if(confirm("Yakin ingin menghapus data?")){
 
-        $.ajax({
+$.ajax({
+url:API_URL+"/"+id,
+type:"DELETE",
 
-            url: API + "/" + id,
-            type:"DELETE",
+success:function(res){
 
-            success:function(response){
+alert(res.message);
 
-                alert(response.message);
-
-                loadData();
-
-            },
-
-            error:function(){
-                alert("Gagal menghapus data");
-            }
-
-        });
-
-    }
+loadData();
 
 }
 
-function clearForm(){
+});
 
-    $("#id").val("");
-    $("#nomorKtp").val("");
-    $("#namaLengkap").val("");
-    $("#alamat").val("");
-    $("#tanggalLahir").val("");
-    $("#jenisKelamin").val("Laki-laki");
+}
+
+}
+
+
+
+function resetForm(){
+
+$("#id").val("");
+$("#nomorKtp").val("");
+$("#namaLengkap").val("");
+$("#alamat").val("");
+$("#tanggalLahir").val("");
+$("#jenisKelamin").val("Laki-laki");
 
 }
